9ea334e959557fe1e901eb5a02621cdbb914d88c
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-06-23 09:59:08 +0500

    implement the animation on the heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import { useGSAP } from "@gsap/react";
@@ -6,6 +5,18 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
+  useGSAP(() => {
+    const text = SplitText.create("#heading", { type: "chars" });
+
+    gsap.from(text.chars, {
+      y: 50,
+      opacity: 0,
+      duration: 1,
+      delay: 0.3,
+      stagger: 0.06,
+      ease: "back.inOut",
+    });
+  }, []);
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
       <h1 id="heading" className="text-5xl font-semibold">
