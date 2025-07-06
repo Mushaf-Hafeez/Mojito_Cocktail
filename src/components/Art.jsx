80400ae7bd9083735ceb0832a704c38e43e4fd11
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-07-06 11:22:42 +0500

    implementing art section

---
+++ src/components/Art.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Art = () => {
+  return (
+    <section className="radial-gradient relative min-h-screen w-full font-poppins">
+      <div className="w-full h-full">
+        <h2
+          id="art-heading"
+          className="text-[14rem] text-center font-semibold font-serif tracking-wider bg-[url('./images/noise.png')]"
+        >
+          The Art
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default Art;
